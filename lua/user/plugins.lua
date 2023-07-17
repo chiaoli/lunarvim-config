054da249812bc219fa97fa0b2596b7eace2f58f5
Chiao-Li Lin <cllin@Chiao-LideMacBook-Pro.local>
2023-07-17 19:24:25 +0800

    Plugin: persistence

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -6,4 +6,14 @@ lvim.plugins = {
       require("todo-comments").setup()
     end
   },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    config = function()
+      require("persistence").setup({
+        dir = vim.fn.expand(vim.fn.stdpath "state" .. "/sessions/"),
+        options = { "buffers", "curdir", "tabpages", "winsize" }
+      })
+    end
+  },
 }
