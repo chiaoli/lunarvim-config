7757dbec1c7a4f1d795bb5ee1b5b20081cf10511
chiaoli <hussar91522@gmail.com>
2023-08-04 21:22:41 +0800

    Plugin: ufo

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -14,3 +14,8 @@ lvim.builtin.project.patterns = {
   "go.mod"
 }
 
+-- fold with ufo
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -73,4 +73,15 @@ lvim.plugins = {
       })
     end
   },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async", },
+    config = function()
+      require("ufo").setup({
+        provider_selector = function()
+          return { 'treesitter', 'indent' }
+        end
+      })
+    end
+  },
 }
