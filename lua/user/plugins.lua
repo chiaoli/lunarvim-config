061645b21a48cbd0ba5903cc84501894e04daa25
chiaoli <hussar91522@gmail.com>
2023-08-04 21:12:23 +0800

    Plugin: hop

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -16,4 +16,25 @@ lvim.plugins = {
       })
     end
   },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      -- keymap see: keymaps.lua
+      local hop = require("hop")
+      local directions = require("hop.hint").HintDirection
+      local opts = { remap = true, silent = true }
+      -- vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      -- vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+      vim.keymap.set("n", "s", ":HopChar1<cr>", opts)
+      vim.keymap.set("n", "S", ":HopWord<cr>", opts)
+      vim.keymap.set("n", "f", function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+      end, opts)
+      vim.keymap.set("n", "F", function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+      end, opts)
+    end,
+  },
 }
