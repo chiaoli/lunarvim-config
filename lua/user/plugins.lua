f29227c13ab43b87e5f33b62198bac7992655e52
chiaoli <hussar91522@gmail.com>
2025-01-10 22:26:41 +0800

    Plugin: cutlass.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -247,4 +247,12 @@ lvim.plugins = {
       })
     end,
   },
+  {
+    "gbprod/cutlass.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or don't set opts to use the default settings
+      -- refer to the configuration section below
+    }
+  },
 }
